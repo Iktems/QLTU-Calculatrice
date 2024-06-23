7a1a6915dde8a9157fae5327428584609a7d6d21
Iktems <114165904+Iktems@users.noreply.github.com>
2024-06-23 19:38:17 +0200

    feat(calculateur): test divide by 0  = sucess

--- src/tests/TDD/calculatrice.jsx
+++ src/tests/TDD/calculatrice.jsx
@@ -13,6 +13,10 @@ class Calculateur {
     }
 
     divide(a, b) {
+        // On ajoute une condition pour éviter la division par zéro
+        if (b === 0) {
+            throw new Error('Division by zero is not allowed');
+        }
         return a / b;
     }
 
