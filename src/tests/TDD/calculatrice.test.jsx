2e60010c440c7f3d0a1cab2f2d142836312ffd58
Iktems <114165904+Iktems@users.noreply.github.com>
2024-06-25 23:22:43 +0200

    fix: playwright correction

--- src/tests/TDD/calculatrice.test.jsx
+++ src/tests/TDD/calculatrice.test.jsx
@@ -1,31 +1,31 @@
-import { describe, test, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import Calculateur from './calculatrice';
 
 describe('Calculateur', () => {
     const calculateur = new Calculateur();
 
     // On test l'addition entre deux nombres pour valider mon test
-    test('Addition de 5 + 5 doit faire 10', () => {
+    it('Addition de 5 + 5 doit faire 10', () => {
         expect(calculateur.add(5, 5)).toBe(10);
     });
 
     // On test la soustraction entre deux nombres pour la validation du test
-    test('Soustraction de 5 - 5 doit faire 0', () => {
+    it('Soustraction de 5 - 5 doit faire 0', () => {
         expect(calculateur.substract(5, 5)).toBe(0);
     });
 
     // On test la multiplication entre deux nombres pour la validation du test
-    test('Multiplication de 5 * 5 doit faire 25', () => {
+    it('Multiplication de 5 * 5 doit faire 25', () => {
         expect(calculateur.multiply(5, 5)).toBe(25);
     });
 
     // On test la division entre deux nombres pour la validation du test
-    test('Division de 5 / 5 doit faire 1', () => {
+    it('Division de 5 / 5 doit faire 1', () => {
         expect(calculateur.divide(5, 5)).toBe(1);
     });
 
     // On test la division par zéro pour la validation du test
-    test('Division par zéro doit retourner une erreur', () => {
+    it('Division par zéro doit retourner une erreur', () => {
         expect(() => calculateur.divide(5, 0)).toThrow();
     });
     
