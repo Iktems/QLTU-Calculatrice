ddcf64aa13d759d62554f41ad6d03fb580b75a6c
Iktems <114165904+Iktems@users.noreply.github.com>
2024-06-26 00:05:05 +0200

    feat(Calculatrice.spec.js): Playwright test for substract, multiply, divide but divide/0 = error

--- src/tests/E2E/tests/calculatrice.spec.js
+++ src/tests/E2E/tests/calculatrice.spec.js
@@ -21,4 +21,49 @@ test.describe('Calculator', () => {
     const result = await page.inputValue('input[type="text"]');
     expect(result).toBe('3');
   });
+
+  test('Il doit soustraire 2 chiffres et donner la bonne réponse', async ({
+    page
+  }) => {
+    await page.click('text=5');
+    await page.click('text=-');
+    await page.click('text=3');
+    await page.click('text==');
+    const result = await page.inputValue('input[type="text"]');
+    expect(result).toBe('2');
+  });
+
+  test('Il doit multiplier 2 chiffres et donner la bonne réponse', async ({
+    page
+  }) => {
+    await page.click('text=5');
+    await page.click('text=x');
+    await page.click('text=2');
+    await page.click('text==');
+    const result = await page.inputValue('input[type="text"]');
+    expect(result).toBe('10');
+  });
+
+  test('Il doit diviser 2 chiffres et donner la bonne réponse', async ({
+    page
+  }) => {
+    await page.click('text=1');
+    await page.click('text=0');
+    await page.click('text=/');
+    await page.click('text=2');
+    await page.click('text==');
+    const result = await page.inputValue('input[type="text"]');
+    expect(result).toBe('5');
+  });
+
+  test('La division par 0 doit donner Infinity', async ({
+    page
+  }) => {
+    await page.click('text=1');
+    await page.click('text=/');
+    await page.click('text=0');
+    await page.click('text==');
+    const result = await page.inputValue('input[type="text"]');
+    expect(result).toBe('Infinity');
+  });
 });
